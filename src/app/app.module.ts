eff82204d6c6b711f65bde1eb68a2c037d6fb0cf
Neeladri16 <mukharjeeneel2000@gmail.com>
2023-09-05 19:08:31 +0530

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,15 @@ import { PromiseComponent } from './promise/promise.component';
 import { ObservableComponent } from './observable/observable.component';
 import { FromEventComponent } from './observable/from-event/from-event.component';
 import { ListComponent } from './observable/list/list.component';
-import { IntervalComponent } from './interval/interval.component';
+import { IntervalComponent } from './observable/interval/interval.component';
+import { OfFromComponent } from './observable/of-from/of-from.component';
+import { ToArrayComponent } from './observable/to-array/to-array.component';
+import { CustomComponent } from './observable/custom/custom.component';
+import { MapComponent } from './observable/map/map.component';
+import { PluckComponent } from './observable/pluck/pluck.component';
+import { FilterComponent } from './observable/filter/filter.component';
+import { TapComponent } from './observable/tap/tap.component';
+import { TakeComponent } from './observable/take/take.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +27,14 @@ import { IntervalComponent } from './interval/interval.component';
     FromEventComponent,
     ListComponent,
     IntervalComponent,
+    OfFromComponent,
+    ToArrayComponent,
+    CustomComponent,
+    MapComponent,
+    PluckComponent,
+    FilterComponent,
+    TapComponent,
+    TakeComponent,
   ],
   imports: [
     BrowserModule,
